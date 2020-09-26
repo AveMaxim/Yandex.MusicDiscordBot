65c39490ba567516f13d891ca82bde87d950cafd
AveMax <maxwwhi@gmail.com>
2020-09-26 18:05:51 +0300

    Test bot, returning messages in terminal

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import discord
 # New coment
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run('NzU4NjM1NTc3ODI5NjIxNzcx.X2x0cA.1dpN3GcF1ZEEc_JGn0f080KoY5g')
