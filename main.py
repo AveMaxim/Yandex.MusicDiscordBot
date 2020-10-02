4576d56f211ab292937af0043861dde1b10867d6
AveMax <maxwwhi@gmail.com>
2020-10-02 18:44:19 +0300

    Bot thath connecting to the channel

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import discord
+from discord.ext import commands
+from discord.utils import get
 import config
+import os
+# from yandex_music.client import Client
 # New coment
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+# client = Client.from_credentials('maxwwhd@yandex.com', 'Afhnjdsq123')
+# client.tracks(['3173236:26637689'])
+bot = commands.Bot(command_prefix=config.BOT_PREFIX)
 
 
-client = MyClient()
-client.run(config.TOKEN)
+@bot.event
+async def on_ready():
+    print("Logged in as: " + bot.user.name + "\n")
+
+
+@bot.command(pass_context=True, aliases=['j', 'joi'])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+
+bot.run(config.TOKEN)
